9f106e36758fb25b61dbc1864d378740b27c38ac
root <root@DESKTOP-V10F18E.>
2024-11-19 18:00:18 -0500

    error, do not run

---
+++ project2/src/components/NavComponents/apiFile.jsx
@@ -0,0 +1,10 @@
+import {Box, Stack, Button} from "@mui/material"
+import {useState, useEffect} from "react"
+
+export function api(){
+    return(
+    <>
+    <Box sx={{width:"120px", color:"blueviolet"}}></Box>
+    </>
+    )
+}
